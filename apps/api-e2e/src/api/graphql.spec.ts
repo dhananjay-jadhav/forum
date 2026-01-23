10569ccb6fe4894c5de8f73a87a5c9642ef1fbae
dhananjay-jadhav <jadhav9260@gmail.com>
2026-01-23 23:27:17 +0530

    Fix e2e test cases

--- apps/api-e2e/src/api/graphql.spec.ts
+++ apps/api-e2e/src/api/graphql.spec.ts
@@ -5,75 +5,19 @@ describe('GraphQL API', () => {
         it('should return the root query', async () => {
             const res = await axios.post('/graphql', {
                 query: `
-                    query {
+                    {
                         query {
-                            id
-                        }
-                    }
-                `,
-            });
-
-            expect(res.status).toBe(200);
-            expect(res.data.errors).toBeUndefined();
-            expect(res.data.data.query).toHaveProperty('id');
-        });
-    });
-
-    describe('Query.node', () => {
-        it('should return null for non-existent node ID', async () => {
-            const res = await axios.post('/graphql', {
-                query: `
-                    query GetNode($id: ID!) {
-                        node(id: $id) {
-                            id
-                        }
-                    }
-                `,
-                variables: {
-                    id: 'WyJxdWVyeSJd', // Base64 encoded non-existent ID
-                },
-            });
-
-            expect(res.status).toBe(200);
-            expect(res.data.errors).toBeUndefined();
-            expect(res.data.data.node).toBeNull();
-        });
-
-        it('should fetch Query node using its ID', async () => {
-            // First get the Query node ID
-            const queryRes = await axios.post('/graphql', {
-                query: `
-                    query {
-                        query {
-                            id
-                        }
-                    }
-                `,
-            });
-
-            const queryNodeId = queryRes.data.data.query.id;
-
-            // Now fetch it using the node query
-            const nodeRes = await axios.post('/graphql', {
-                query: `
-                    query GetNode($id: ID!) {
-                        node(id: $id) {
-                            id
-                            ... on Query {
-                                __typename
+                            query{
+                                nodeId
                             }
                         }
                     }
                 `,
-                variables: {
-                    id: queryNodeId,
-                },
             });
 
-            expect(nodeRes.status).toBe(200);
-            expect(nodeRes.data.errors).toBeUndefined();
-            expect(nodeRes.data.data.node).toHaveProperty('id', queryNodeId);
-            expect(nodeRes.data.data.node).toHaveProperty('__typename', 'Query');
+            expect(res.status).toBe(200);
+            expect(res.data.errors).toBeUndefined();
+            expect(res.data.data.query.query).toHaveProperty('nodeId');
         });
     });
 
@@ -126,7 +70,7 @@ describe('GraphQL API', () => {
                 name: 'Node',
                 fields: [
                     {
-                        name: 'id',
+                        name: 'nodeId',
                         type: {
                             name: null,
                             kind: 'NON_NULL',
